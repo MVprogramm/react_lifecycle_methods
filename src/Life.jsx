f290c02db22f124c5739f77b0c70682fc06c5d9b
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-06-06 00:20:14 +0300

    fix errors

--- src/Life.jsx
+++ src/Life.jsx
@@ -8,7 +8,7 @@ class Life extends React.Component {
   }
 
   componentDidMount() {
-    console.log('compponentDidMount: API calls, subscriptions');
+    console.log('componentDidMount: API calls, subscriptions');
   }
 
   render() {
