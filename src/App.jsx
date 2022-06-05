a5c46a6fc03d292f45c072b2b79857e47ee6eb68
Michael Valiahmetov <michael.valiahmetov@gmail.com>
2022-06-06 01:59:46 +0300

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,49 @@ import './index.scss';
 
 import Life from './Life.jsx';
 
-const App = () => {
-  return <Life />
+class App extends React.Component {
+  state = {
+    rendering: true,
+    isUpdate: false,
+  }
+
+  show = () => {
+    this.setState({
+      rendering: true,
+    })
+  }
+
+  hide = () => {
+    this.setState({
+      rendering: false,
+    })
+  }
+
+  update = () => {
+    this.setState({
+      isUpdate: true,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="btns">
+          <button className="btn" onClick={this.show}>
+            Show
+          </button>
+          <button className="btn" onClick={this.hide}>
+            Hide
+          </button>
+          <button className="btn" onClick={this.update}>
+            Update
+          </button>
+        </div>
+        
+        {<Life isUpdate={this.state.isUpdate}/>}
+      </div>
+    )
+  }    
 }
 
 export default App;
--- src/Life.jsx
+++ src/Life.jsx
@@ -11,6 +11,12 @@ class Life extends React.Component {
     console.log('componentDidMount: API calls, subscriptions');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+
+    return true;
+  }
+
   render() {
     return null;
   }
